558e14182bca3914a99ad6d07386cdbf103eaa0f
Ysaac Andujar <78627655+Tyrone09876543@users.noreply.github.com>
2022-10-07 11:06:34 -0400

    db context and brand entity

---
+++ CarsApi/Entities/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarsApi.Entities
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
